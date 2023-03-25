24b3537fc570b87345b8e58fd2c29c316e9e98b9
SAKINA AZAM <111923897+aounjabbarhashmi@users.noreply.github.com>
2023-03-25 05:06:24 +0500

    Add templates in AboutUs

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,71 +1,211 @@
 <template>
- <div class>
-     <div class="w-full min-h-[600px] bg-red-400 bgImg">
+  <div class>
+    <div class="w-full min-h-[600px] bg-red-400 bgImg">
       <div class="w-full h-[600px] flex justify-center items-center">
         <div class="w-[60%]">
-          <h1 class="text-5xl text-white font-normal tracking-wide w-[50%] leading-snug">
+          <h1
+            class="text-5xl text-white font-normal tracking-wide w-[50%] leading-snug"
+          >
             <span>Our Story</span>
           </h1>
           <p
             class="text-white mt-4 tracking-wide leading-snug font-semibold w-[50%]"
-          >Short Introductory summary for the page<br>goes here lilke so</p>
+          >
+            Short Introductory summary for the page<br />goes here lilke so
+          </p>
         </div>
       </div>
     </div>
 
     <!-- Step 2 -->
-  <div class="w-full flex justify-center">
-    <div class="w-[60%]">
-    
-      <div class="py-40">
-      <div class="max-w-5xl  grid md:grid-cols-2 grid-cols-1 h-[540px] gap-12">
-        <div class>
-          <h1 class="text-4xl mt-12 text-trisec leading-[50px] font-semibold">
-            <span class="font-light">Working towards a world</span>
-            <span> without homelessness</span>
-          </h1>
-          <p
-            class="text-md mt-8 leading-loose tracking-wide"
-          >At Stef & Philips, we strive to make the lives of vulnerable people safer and more secure. We do this by delivering safe, suitable homes and vital services for tenants, authorities and partners, while providing secure investments for private and corporate investors.</p>
+    <div class="w-full flex justify-center">
+      <div class="w-[60%]">
+        <div class="py-40">
+          <div
+            class="max-w-5xl grid md:grid-cols-2 grid-cols-1 h-[540px] gap-12"
+          >
+            <div class>
+              <h1
+                class="text-4xl mt-12 text-trisec leading-[50px] font-semibold"
+              >
+                <span class="font-light">Working towards a world</span>
+                <span> without homelessness</span>
+              </h1>
+              <p class="text-md mt-8 leading-loose tracking-wide">
+                At Stef & Philips, we strive to make the lives of vulnerable
+                people safer and more secure. We do this by delivering safe,
+                suitable homes and vital services for tenants, authorities and
+                partners, while providing secure investments for private and
+                corporate investors.
+              </p>
+            </div>
+            <div class="flex justify-center">
+              <img src="../assets/HeroSection.png" alt />
+            </div>
+          </div>
         </div>
-        <div class="flex justify-center ">
-          <img src="../assets/HeroSection.png" alt >
-        </div>
-      </div>
-    </div>
-    <Cards />
+        <Cards />
 
-    <!-- step-2 -->
-    <div class=" py-22">
-      <div class="grid videoBg md:grid-cols-2 grid-cols-1 max-w-5xl mx-auto">
-        <div class="flex items-center justify-center">
-          <div class="w-4/5 mt-3 md:mt-0 ">
-           <div class="relative">
-  <iframe class=" w-max  aspect-video lg:aspect-square   ml-48" src="https://www.youtube-nocookie.com/embed/FMrtSHAAPhM" ></iframe>
-</div>
+        <!-- step-2 -->
+        <div class="py-22">
+          <div
+            class="grid videoBg md:grid-cols-2 grid-cols-1 max-w-5xl mx-auto"
+          >
+            <div class="flex items-center justify-center">
+              <div class="w-4/5 mt-3 md:mt-0">
+                <div class="relative">
+                  <iframe
+                    class="w-max aspect-video lg:aspect-square ml-48"
+                    src="https://www.youtube-nocookie.com/embed/FMrtSHAAPhM"
+                  ></iframe>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>    
+    </div>
+    <!-- image -->
+    <div class="happyG w-full h-[600px] relative overflow-hidden py-40 mt-52">
+      <div
+        class="absolute top-0 w-full h-full flex items-center flex-wrap justify-center left-0 happyS"
+      >
+          <div class="w-full flex justify-center">
+      <div class="w-[60%]">
+        <div class="max-w-4xl w-full mx-auto grid">
+          <div class="flex ">
+            <div class="mt-4 flex-auto px-8 text-white md:font-semibold">
+              Since first opening our doors in 2006, Stef & Philips has evolved
+              from a traditional estate agency into a leading provider of
+              accommodation services around the UK. We work closely with local
+              authorities, housing associations, charitable organisations and
+              investors to provide safe, suitable and secure accommodation to
+              those who really need it..
+            </div>
+            <div class="mt-4 px-8 flex-auto text-white md:font-semibold">
+              We construct and manage fit-for-purpose properties in areas of
+              high demand, providing tangible solutions to the current housing
+              crisis and giving vulnerable people a lifeline and an opportunity
+              to get back on their feet. Our care reaches beyond bricks and
+              mortar, to include specialised accommodation and services for
+              mothers and babies, tenants with disabilities, and young adults
+              living on their own for the first time.
+            </div>
+          </div>
+        </div>
+      </div>
+          </div>
+      </div>
+    </div>
+  
+  <!-- step 4 -->
+      <div class="w-full flex justify-center">
+      <div class="w-[60%]">
+  <div class="mt-44">
+    <div class="flex ">
+      <div class="flex-auto ">
+        <img class="max-w-sm" src="../assets/cardImage.png" alt />
+      </div>
+     <div class="flex-auto">
+              <h1
+                class="text-4xl mt-10 px-8 text-trisec text-ellipsis leading-[40px] font-semibold"
+              >
+                <span class="font-light">Why  </span>
+                <span>social housing investment </span><span class="font-light">matters to us</span>
+              </h1>
+              <p class="text-sm mt-4 px-8">
+              Everyone deserves to have a safe place to call home. Yet, the UK’s
+               housing crisis is getting worse: homelessness in England is forecast
+               to continue to grow – and almost double – over the next 25 years.
+              </p>
+            </div>
+    </div>
+      <div class="flex ">
+     <div class="flex-auto max-w-2xl w-full mx-auto grid ">
+              <p class="text-sm   mt-20 px-8 ">
+              More and more accommodation is being deemed unsuitable for housing
+              the homeless and vulnerable.The National Housing Federation says,
+              “England alone needs 340,000 new homes built a year, including 145,000
+              social and affordable homes but we are not achieving targets.”In London
+              alone, 60,000 homes are currently needed; by 2041,this will have risen to
+              184,400. Alongside this,we are seeing a constant increase in rough 
+              sleepingand a dramatic rise in unsuitabl emporary accommodation
+              The most vulnerablemembers of our society need help</p>
+            </div>
+              <div class="flex-auto">
+        <img class="max-w-sm" src="../assets/ncardImage.png" alt />
+      </div>
     </div>
   </div>
-<!-- image -->
-    <div class="happyG w-full h-[600px] relative overflow-hidden py-40 ">
-      <div class="absolute top-0 w-full h-full flex items-center  flex-wrap justify-center left-0 happyS">
-        <div class="max-w-5xl w-full mx-auto grid grid-cols-1 ">
-            <div class="flex">
-              <div
-                class="mt-4 flex-auto px-8  text-white md:font-semibold "
-              >Since first opening our doors in 2006, Stef & Philips has evolved from a traditional estate agency into a leading provider of accommodation services around the UK. We work closely with local authorities, housing associations, charitable organisations and investors to provide safe, suitable and secure accommodation to those who really need it..</div>
-              <div  class="mt-4  px-8 flex-auto  text-white md:font-semibold ">We construct and manage fit-for-purpose properties in areas of high demand, providing tangible solutions to the current housing crisis and giving vulnerable people a lifeline and an opportunity to get back on their feet. Our care reaches beyond bricks and mortar, to include specialised accommodation and services for mothers and babies, tenants with disabilities, and young adults living on their own for the first time.</div>
+      </div>
+      </div>
 
-            </div>
-                
+      <!-- step 5 -->
+       
+       <div class="bg-second  pb-80 mt-1">
+       <div class="w-full flex justify-center">
+      <div class="w-[60%]">
+        <div class="grid md:grid-cols-3 gap-12 grid-cols-1 sm:grid-cols-2 mt-16">
+        <h1 class="text-white text-4xl">Value</h1>
+          <div class>
+            <img  class="w-20" src="../assets/blockM.png" alt />
+            <h1 class="py-2 text-white font-bold text-2xl">Value</h1>
+            <p
+              class="text-white font-semibold text-md"
+            >Short description goes here like so in this featured icon element lore ipsum.</p>
           </div>
-     
+          <div class>
+            <img class="w-20" src="../assets/blockN.png" alt />
+            <h1 class="py-2 text-white font-bold text-2xl">Value</h1>
+            <p
+              class="text-white font-semibold text-md"
+            >Short description goes here like so in this featured icon
+             element lore ipsum short description goes here like so in
+             this featured icon element lore ipsum.</p>
+          </div>
+         
+        </div>
       </div>
     </div>
- </div>
+    <div class="flex max-w-4xl w-full  float-right  mt-10">
+     <div class>
+            <img  class="w-20" src="../assets/blockM.png" alt />
+            <h1 class="py-2 text-white font-bold text-2xl">Value</h1>
+            <p
+              class="text-white font-semibold text-md"
+            >Short description goes here like so in this featured icon element lore ipsum.</p>
+          </div>
+          <div class>
+            <img  class="w-20" src="../assets/blockM.png" alt />
+            <h1 class="py-2 text-white font-bold text-2xl">Value</h1>
+            <p
+              class="text-white font-semibold text-md"
+            >Short description goes here like so<br>
+             in this featured icon element lore  ipsum  short description goes
+             here like</p>
+          </div>
+          <div class>
+            <img  class="w-20" src="../assets/blockM.png" alt />
+            <h1 class="py-2 text-white font-bold text-2xl">Value</h1>
+            <p
+              class="text-white font-semibold text-md"
+            >Short description goes here like so in this featured icon element lore ipsum.</p>
+          </div>
+      </div>
+      
+    </div>
+    <!-- step 6 -->
+    
+       <div class="w-full flex justify-center">
+      <div class="w-[60%]">
+          <div class=" blueBg py-48 mt-24">
+      </div>
+       </div>
+     </div>
+    
+  </div>
+
+
 </template>
 
 <style >
@@ -75,7 +215,7 @@
   background-size: cover;
   background-position: center;
 }
-.videoBg{
+.videoBg {
   background-image: url("../assets/videoBg.png");
   background-repeat: no-repeat;
   background-size: cover;
@@ -89,6 +229,12 @@
 }
 .happyS {
   background-image: url("../assets/bggray.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.blueBg{
+  background-image: url("../assets/blueBg.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
